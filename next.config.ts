870363ffa23eb887ea60436a1b7b381342d41ecb
Your Name <you@example.com>
2025-11-14 08:25:37 +0000

    Config ESlint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  eslint: {
+    ignoreDuringBuilds: true, // Désactive ESLint pendant `npm run build`
+  }, // ← Fin config ESLint (à retirer quand tu voudras réactiver les règles)
 };
 
 export default nextConfig;
